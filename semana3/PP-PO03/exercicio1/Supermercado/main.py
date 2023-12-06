5723a71bc033a87dbe60e804211c78685cec333e
Ian Rodrigues Alexandrino <133264089+IanAlexandrino@users.noreply.github.com>
2023-12-06 05:21:36 -0300

    Adicionando menu

--- semana3/PP-PO03/exercicio1/Supermercado/main.py
+++ semana3/PP-PO03/exercicio1/Supermercado/main.py
@@ -1,7 +1,15 @@
 
 
 def main():
-    pass
+     while True:
+        print("Menu de Opções:")
+        print("1. Inserir novo produto")
+        print("2. Excluir produto cadastrado")
+        print("3. Listar todos os produtos")
+        print("4. Consultar preço de um produto")
+        print("0. Sair")
+
+        opcao = input("Escolha uma opção: ")
 
 
 if __name__ == "__main__":
